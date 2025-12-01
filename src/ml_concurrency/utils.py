8b0d1e94ac082eec2c98fe4701ea4362cec84a2c
JorgeIvanJH <jorgeivan.jaramillo.herrera@gmail.com>
2025-12-01 15:56:13 +0000

    true tracking of parallel processes

--- src/ml_concurrency/utils.py
+++ src/ml_concurrency/utils.py
@@ -8,6 +8,9 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 import numpy as np
 import time
 from datetime import datetime
+import threading
+import psutil
+
 
 def load_dataset(DATASET_PATH, NUM_ROWS):
     """Load and preprocess the healthcare no-shows dataset"""
@@ -28,35 +31,66 @@ def load_dataset(DATASET_PATH, NUM_ROWS):
     return X, y
 
 
-def measure_performance(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
+def measure_performance(include_children=False, sample_interval=0.01):
+    """
+    Decorator to measure execution time and peak RSS memory (in MB).
 
-        # BEGIN TIME AND MEMORY MEASUREMENT
-        tracemalloc.start()
-        start_snapshot = tracemalloc.take_snapshot()
-        start = time.perf_counter()
-        
-        result = func(*args, **kwargs)
-        
-        # END TIME AND MEMORY MEASUREMENT
-        end = time.perf_counter()
-        current, peak = tracemalloc.get_traced_memory()
-        tracemalloc.stop()
+    - include_children=True will also include memory from child processes,
+      which is what you want for parallel / ProcessPoolExecutor code.
+    - sample_interval is how often (in seconds) memory is sampled.
+    """
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            proc = psutil.Process(os.getpid())
+            stop_event = threading.Event()
+            peak_bytes = 0
 
-        # COMPUTE TIME AND MEMORY USAGE
-        execution_time = end - start
-        peak_mb = peak / (1024 ** 2)
-        return result, execution_time, peak_mb
-    return wrapper
+            def memory_sampler():
+                nonlocal peak_bytes
+                while not stop_event.is_set():
+                    try:
+                        rss = proc.memory_info().rss
+                        if include_children:
+                            for child in proc.children(recursive=True):
+                                try:
+                                    rss += child.memory_info().rss
+                                except (psutil.NoSuchProcess, psutil.AccessDenied):
+                                    pass
+                        if rss > peak_bytes:
+                            peak_bytes = rss
+                    except (psutil.NoSuchProcess, psutil.AccessDenied):
+                        break
+                    time.sleep(sample_interval)
 
+            # Start memory sampler thread
+            sampler_thread = threading.Thread(target=memory_sampler)
+            sampler_thread.daemon = True
+            sampler_thread.start()
 
-@measure_performance
+            # Time + function execution
+            start = time.perf_counter()
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                end = time.perf_counter()
+                # Stop sampler and wait for it to finish
+                stop_event.set()
+                sampler_thread.join()
+
+            execution_time = end - start
+            peak_mb = peak_bytes / (1024 ** 2)
+            return result, execution_time, peak_mb
+
+        return wrapper
+    return decorator
+
+@measure_performance(include_children=False)
 def sequential_execution(model, X, y):
     y_hat = model.predict(X)
     return y_hat
 
-@measure_performance
+@measure_performance(include_children=True)
 def parallel_execution(model, X, y, n_jobs=-1):
 
     num_rows = X.shape[0]
