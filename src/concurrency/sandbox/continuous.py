ffc398ac78fe67d39e06813496e550a042c3418a
JorgeIvanJH <jorgeivan.jaramillo.herrera@gmail.com>
2025-12-04 17:25:09 +0000

    query parallelization demo

---
+++ src/concurrency/sandbox/continuous.py
@@ -0,0 +1,151 @@
+import multiprocessing as mp
+from collections import Counter
+import time
+import numpy as np
+TIMEPERDAY = 1.0  # seconds to simulate query time per day
+
+def measure_time_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        return result, elapsed_time
+    return wrapper
+
+def split_into_workers(lst, num_workers):
+    """
+    divides lst into num_workers sublists of (approximately) equal size
+    """
+    k, m = divmod(len(lst), num_workers)
+    return [lst[i*k + min(i, m) : (i+1)*k + min(i+1, m)] for i in range(num_workers)]
+
+def query(day, query_time=TIMEPERDAY):
+    """
+    Simulate query of data for a given day
+    """
+    time.sleep(query_time)
+    return "batch_day_" + str(day)
+
+def batch_query(days):
+    """
+    simulate batch query of data for multiple days
+    """
+    numdays = len(days)
+    query_time = TIMEPERDAY + np.sqrt(numdays)
+    time.sleep(query_time) # TODO: Adjust to a more realistic approach
+    return ["batch_day_" + str(day) for day in days]
+
+def model(data,inference_time=0.1):
+    """
+    Simulate inference on a model
+    """
+    time.sleep(inference_time)
+    return f"processed_{data}"
+
+def worker(raw_data_queue, day):
+    raw_data = query(day)
+    raw_data_queue.put(raw_data)
+
+def batch_worker(raw_data_queue, days_batch):
+    raw_data = batch_query(days_batch)
+    for data in raw_data:
+        raw_data_queue.put(data)
+
+@measure_time_decorator
+def sequential_approach(days):
+    """
+    query and model are both called sequentially
+    """
+    processed_data = []
+    for day in days:
+        raw_data = query(day)
+        processed = model(raw_data)
+        processed_data.append(processed)
+    return processed_data
+
+@measure_time_decorator
+def approach_1(days):
+    """
+    query is called in parallel, 1 process per day
+    model is called sequentially
+    """
+    raw_data_queue = mp.Queue()
+    processes = []
+    for day in days:
+        p = mp.Process(target=worker, args=(raw_data_queue, day))
+        processes.append(p)
+        p.start()
+    for p in processes: # Wait for all processes to complete
+        p.join()
+    processed_data = []
+    while not raw_data_queue.empty():
+        raw_data = raw_data_queue.get()
+        processed = model(raw_data)
+        processed_data.append(processed)
+    return processed_data
+
+@measure_time_decorator
+def approach_2(days,num_workers=4):
+    """
+    query is called in parallel batches, [num_workers] processes
+    model is called sequentially
+    """
+    days_batches = split_into_workers(days, num_workers)
+    raw_data_queue = mp.Queue()
+    processes = []
+    for days_batch in days_batches:
+        p = mp.Process(target=batch_worker, args=(raw_data_queue, days_batch))
+        processes.append(p)
+        p.start()
+    for p in processes: # Wait for all processes to complete
+        p.join()
+    processed_data = []
+    while not raw_data_queue.empty():
+        raw_data = raw_data_queue.get()
+        processed = model(raw_data)
+        processed_data.append(processed)
+    return processed_data
+
+@measure_time_decorator
+def approach_3(days, num_workers=4):
+    """
+    query is called in parallel batches, [num_workers] processes
+    model is called asynchronously after each data batch is retrieved
+
+    """
+    days_batches = split_into_workers(days, num_workers)
+    raw_data_queue = mp.Queue()
+    processes = []
+    for days_batch in days_batches:
+        p = mp.Process(target=batch_worker, args=(raw_data_queue, days_batch))
+        processes.append(p)
+        p.start()
+
+    processed_data = []
+
+    for _ in range(len(days_batches)):
+        raw_data = raw_data_queue.get()    # blocks until one batch is ready
+        processed = model(raw_data)        # run model immediately (as_completed)
+        processed_data.append(processed)
+    for p in processes: # Wait for all processes to complete
+        p.join()
+
+    return processed_data
+
+if __name__ == '__main__':
+
+    days = list(range(1, 30))
+
+    methods = [sequential_approach, approach_1, approach_2, approach_3]
+
+    for method in methods:
+        processed_data, elapsed_time = method(days)
+        print(f"method: {method.__name__}, time: {elapsed_time:.2f} seconds")
+        
+        # VERIFY CONSISTENCY ACROSS METHODS
+        previous_processed_data = None
+        if previous_processed_data is not None:
+            assert Counter(previous_processed_data) == Counter(processed_data)
+        previous_processed_data = processed_data
+
